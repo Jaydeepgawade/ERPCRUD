345784fcf84488490c65a33eccc2017016cb189f
Jaydeep Gawade <jaydeepgawade42@gmail.com>
2025-05-17 16:36:26 +0530

    Initial commit - ERP CRUD project without ignored files

---
+++ ERP_CRUD_REDIS_JWT_LOG/ERP_CRUD_REDIS_JWT_LOG/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using ERP_CRUD_REDIS_JWT_LOG.Models;
+
+namespace ERP_CRUD_REDIS_JWT_LOG.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ ERP_CRUD_REDIS_JWT_LOG/ERP_CRUD_REDIS_JWT_LOG/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ERP_CRUD_REDIS_JWT_LOG.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
